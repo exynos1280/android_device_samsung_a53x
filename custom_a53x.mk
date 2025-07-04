88c8a292a2f7aa31a6483a0b6fb0b92218247d99
Majaahh <maja.blaszczak@proton.me>
2025-07-04 16:47:19 +0200

    a53x: Adapt to PixelOS
    Change-Id: If8b11aa88c2cf099b96c5dffb6229a999df2fbdf
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- custom_a53x.mk
+++ custom_a53x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) The LineageOS Project
+# Copyright (C) The PixelOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,18 +17,21 @@
 # Inherit from a53x device
 $(call inherit-product, device/samsung/a53x/device.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := a53x
-PRODUCT_NAME := lineage_a53x
+PRODUCT_NAME := custom_a53x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A536B
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 34
 BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_CHARACTERISTICS := phone
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="a53xnaxx-user 15 AP3A.240905.015.A2 A536BXXSHFYI1 release-keys" \
