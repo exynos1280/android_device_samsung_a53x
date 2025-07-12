5f145038f3603d6d6207adca795920e99f066a8f
Flopster101 <nahuelgomez329@gmail.com>
2025-07-12 18:53:35 -0300

    a53x: props: Set model name dynamically
    * Inspired by https://github.com/RedmiNote10S/android_device_redmi_rosemary/tree/pixelexperience/thirteen/props
    
    Co-authored-by: Myst33d <notmyst33d@gmail.com>
    Co-authored-by: Bahar KURT <kurtbahartr@users.noreply.github.com>
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,4 +25,8 @@ DEVICE_PATH := device/samsung/a53x
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
