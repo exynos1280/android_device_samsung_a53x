c93baa619bf66c5f7263989f2702f50bc3a26904
Flopster101 <nahuelgomez329@gmail.com>
2025-07-14 02:29:40 -0300

    a53x: audio: Merge mixer_gains with mixer_paths
    Required by oss audio hal.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ DEVICE_PATH := device/samsung/a53x
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_board_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_board_info.xml \
-    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # init
