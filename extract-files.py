fd7513406c4d652d737d18ae96fdfe1cea450d8a
ChrisCatto <chriscatto@proton.me>
2025-07-15 19:08:34 +0200

    a53x: Refactor keymint service patching
    * There is an issue with keymint V4 NDK in our device because of some
      changes to keystore2 related to setAdditionalAttestationInfo and
      after Android 16 release and it seems like we cannot boot by keep
      using it. Downgrading keymint NDK to V3 allows us to boot again.
    
    * However, by doing this, checkelf must be disabled for keymint service
      the same goes for dependencies generation in Android.bp (for this
      service only).
    
    Change-Id: Iefdb624502ca9bd78881c0e542a2a8f5584a8be7
    Signed-off-by: ChrisCatto <chriscatto@proton.me>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -28,7 +28,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libvkservice.so': blob_fixup()
         .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
     (
-        'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
         'vendor/lib64/libskeymint10device.so',
         'vendor/lib64/libskeymint_cli.so',
     ): blob_fixup()
@@ -38,6 +37,21 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.security.keymint-V4-ndk')
         .replace_needed('android.hardware.security.keymint-V1-ndk',
             'android.hardware.security.keymint-V4-ndk')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
+            'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
+             'android.hardware.security.sharedsecret-V1-ndk.so')
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
+        .replace_needed('libssl.so', 'libssl-tm.so')
+        .add_needed('libshim_crypto.so'),
+    'vendor/bin/hw/android.hardware.security.keymint-service.samsung': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
+            'android.hardware.security.keymint-V3-ndk.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform',
+            'android.hardware.security.keymint-V3-ndk')
+        .replace_needed('android.hardware.security.keymint-V1-ndk',
+            'android.hardware.security.keymint-V3-ndk')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
             'android.hardware.security.secureclock-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
